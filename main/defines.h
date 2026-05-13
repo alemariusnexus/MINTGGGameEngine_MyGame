5fe50584f14d90c57767ecfb40b9422e09edd472
alemariusnexus <david@dlerch.de>
2026-05-13 11:55:34 +0200

    Engine setup now mostly happens in the engine itself

--- main/defines.h
+++ main/defines.h
@@ -1,3 +1,9 @@
 #pragma once
 
-#include "defines_esp32.h"
+#ifdef CONFIG_IDF_TARGET_ESP32C3
+#   include "defines_esp32c3.h"
+#elif defined(CONFIG_IDF_TARGET_ESP32)
+#   include "defines_esp32.h"
+#else
+#   error Unknown ESP32 device for defines.h!
+#endif
